d0f701c5ae3f0155fe46ed3566a001774aea5f62
Maks <maksdpua@gmail.com>
2016-05-12 17:31:43 +0300

    start

---
+++ SwiftARC/main.swift
@@ -0,0 +1,52 @@
+//
+//  main.swift
+//  SwiftARC
+//
+//  Created by Maks on 5/12/16.
+//  Copyright © 2016 Maks. All rights reserved.
+//
+
+import Foundation
+
+var playground = true
+
+class Student {
+    
+    var teacher : Teacher?
+    
+    deinit {
+        print("goodbye student")
+    }
+    
+}
+
+
+class Teacher {
+    
+    var student : Student?
+    
+    deinit {
+        print("goodbye teacher")
+    }
+    
+}
+
+//var t : Teacher?
+
+if playground {
+    
+    var teacher = Teacher()
+    
+    
+    if playground {
+        var student = Student()
+        teacher.student = student
+    }
+    
+    
+    print("exit playground")
+}
+
+print("end")
+
+
